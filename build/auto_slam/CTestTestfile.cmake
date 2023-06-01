13b144faf749a0049d9612b46ee7227340f7733c
ChrisLye <christpher.zijian@gmail.com>
2023-06-01 17:01:01 +0900

    Skeleton file for auto_slam

--- build/auto_slam/CTestTestfile.cmake
+++ build/auto_slam/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/chris/catkin_ws/src
-# Build directory: /home/chris/catkin_ws/build
+# Source directory: /home/chris/catkin_ws/src/auto_slam
+# Build directory: /home/chris/catkin_ws/build/auto_slam
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("auto_slam")
---
+++ src/auto_slam/src/startup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+
+def start():
+    print("Auto SLAM module is running..")
+
+if __name__ == '__main__':
+    start()
